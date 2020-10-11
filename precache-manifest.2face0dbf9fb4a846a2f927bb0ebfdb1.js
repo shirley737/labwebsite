755fe2f9742b609afe83e7abf2007789aca58846
Shirley Guo <shirley_guo@intuit.com>
2020-10-10 21:35:22 -0700

    Updates

---
+++ precache-manifest.2face0dbf9fb4a846a2f927bb0ebfdb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0378b4fc5ad9f88e773c835cefd4c1f",
+    "url": "/labwebsite/index.html"
+  },
+  {
+    "revision": "db97002021372c95fe27",
+    "url": "/labwebsite/static/css/main.460aa0ad.chunk.css"
+  },
+  {
+    "revision": "0b3536d56f56b57fb5b8",
+    "url": "/labwebsite/static/js/2.513b27f1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/labwebsite/static/js/2.513b27f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db97002021372c95fe27",
+    "url": "/labwebsite/static/js/main.7f13cd4b.chunk.js"
+  },
+  {
+    "revision": "82c28e9ddd5f5070ac0c",
+    "url": "/labwebsite/static/js/runtime-main.90196989.js"
+  },
+  {
+    "revision": "dec462321332b55bae8d008dd3bc0a92",
+    "url": "/labwebsite/static/media/banner.dec46232.jpg"
+  },
+  {
+    "revision": "25c6007fd9f018b48342bf9c92842515",
+    "url": "/labwebsite/static/media/image1.25c6007f.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/labwebsite/precache-manifest.052789d56a5eb8e29a36b0513f99adcf.js"
+  "/labwebsite/precache-manifest.2face0dbf9fb4a846a2f927bb0ebfdb1.js"
 );
 
 self.addEventListener('message', (event) => {
